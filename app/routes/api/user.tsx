af465eb50411e83a2aa3c902c485e3e67efef378
Surendar Yama <surendaryama@outlook.com>
2025-03-06 14:49:43 +0530

    Remix (React Router) GET init

---
+++ app/routes/api/user.tsx
@@ -0,0 +1,7 @@
+export async function loader(){
+    return {
+        name: "Surendar Yama",
+        age:32,
+        isAuthenticated: true,
+    }
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import {useLoaderData} from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +8,24 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export async function clientLoader (){
+  try {
+    const data = await fetch("http://localhost:5174/user");
+    return await data.json();
+  }catch(error){
+    return {error: "error thrown..."};
+  }
 }
+
+export default function Home(){
+  const {name,age,isAuthenticated} = useLoaderData();
+
+  return (<>
+    <div className="flex flex-col bg-purple-200 text-black p-10 space-y-5">
+      <span>Name : {name}</span>
+      <span>Age: {age}</span>
+      <span>isAuthenticated: {isAuthenticated ? "Yes" : "No"}</span>
+    </div>
+  </>)
+}
+
